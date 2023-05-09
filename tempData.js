a1949fcbb1c317cd14975c2a1fb6e7adab1472fc
Anglebert-Dev <anglebertsh@gmail.com>
2023-05-09 23:29:42 +0200

    initial commit

---
+++ tempData.js
@@ -0,0 +1,74 @@
+export default todosData = [
+    {
+      name: "Plan a Trip",
+      color: "#24A6D9",
+      todos: [
+        {
+          title: "Book Flight",
+          completed: false,
+        },
+        {
+          title: "Passport Check",
+          completed: true,
+        },
+        {
+          title: "Reserve Hotel Room",
+          completed: true,
+        },
+        {
+          title: "Pack Luggage",
+          completed: true,
+        },
+      ],
+    },
+    {
+      name: "Work on Project",
+      color: "#9B1231",
+      todos: [
+        {
+          title: "Gather Requirements",
+          completed: true,
+        },
+        {
+          title: "Design Prototype",
+          completed: true,
+        },
+        {
+          title: "Develop Frontend",
+          completed: false,
+        },
+        {
+          title: "Test and Debug",
+          completed: false,
+        },
+        {
+          title: "Deploy to Production",
+          completed: false,
+        },
+      ],
+    },
+    {
+      name: "House Chores",
+      color: "#FFAC33",
+      todos: [
+        {
+          title: "Clean the Kitchen",
+          completed: true,
+        },
+        {
+          title: "Do Laundry",
+          completed: false,
+        },
+        {
+          title: "Mow the Lawn",
+          completed: false,
+        },
+        {
+          title: "Organize Closet",
+          completed: false,
+        },
+      ],
+    },
+    // Add more todo lists as needed
+  ];
+  
